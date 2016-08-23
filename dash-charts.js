03be09b3529fba1750eaacddddd8cd101b89af53
jackwrx <jackwrx@gmail.com>
2016-08-23 14:23:56 +0800

    Add files via upload

---
+++ dash-charts.js
@@ -0,0 +1,113 @@
+/*** First Chart in Dashboard page ***/
+
+	$(document).ready(function() {
+        var NetworkFailed = localStorage.getItem("NetworkFailed");
+        var TotalServers = localStorage.getItem("TotalServers");
+        var Percent = (eval(NetworkFailed)/eval(TotalServers)) * 100;
+        var Ratio = 100 - Percent;
+      
+        $('h2:first').html("<h2>"+ eval(Percent.toFixed(1)) + "%</h2>");
+        
+		info = new Highcharts.Chart({
+			chart: {
+				renderTo: 'load',
+				margin: [0, 0, 0, 0],
+				backgroundColor: null,
+                plotBackgroundColor: 'none',
+							
+			},
+			
+			title: {
+				text: null
+			},
+
+			tooltip: {
+				formatter: function() { 
+					return this.point.name +': '+ this.y +' %';
+						
+				} 	
+			},
+            series: [
+				{
+				borderWidth: 2,
+				borderColor: '#F1F3EB',
+				shadow: false,	
+				type: 'pie',
+				name: 'Income',
+				innerSize: '65%',
+				data: [
+					{ name: 'load percentage', y: eval(Percent.toFixed(1)), color: '#b2c831' },
+					{ name: 'rest', y: eval(Ratio.toFixed(1)), color: '#3d3d3d' }
+				],
+				dataLabels: {
+					enabled: false,
+					color: '#000000',
+					connectorColor: '#000000'
+				}
+			}]
+		    /*series: [
+				{
+				borderWidth: 2,
+				borderColor: '#F1F3EB',
+				shadow: false,	
+				type: 'pie',
+				name: 'Income',
+				innerSize: '65%',
+				data: [
+					{ name: 'load percentage', y: 3.0, color: '#b2c831' },
+					{ name: 'rest', y: 97.0, color: '#3d3d3d' }
+				],
+				dataLabels: {
+					enabled: false,
+					color: '#000000',
+					connectorColor: '#000000'
+				}
+			}]*/
+		});
+		
+	});
+
+/*** second Chart in Dashboard page ***/
+
+	$(document).ready(function() {
+		info = new Highcharts.Chart({
+			chart: {
+				renderTo: 'space',
+				margin: [0, 0, 0, 0],
+				backgroundColor: null,
+                plotBackgroundColor: 'none',
+							
+			},
+			
+			title: {
+				text: null
+			},
+
+			tooltip: {
+				formatter: function() { 
+					return this.point.name +': '+ this.y +' %';
+						
+				} 	
+			},
+		    series: [
+				{
+				borderWidth: 2,
+				borderColor: '#F1F3EB',
+				shadow: false,	
+				type: 'pie',
+				name: 'SiteInfo',
+				innerSize: '65%',
+				data: [
+					{ name: 'Used', y: 65.0, color: '#fa1d2d' },
+					{ name: 'Rest', y: 35.0, color: '#3d3d3d' }
+				],
+				dataLabels: {
+					enabled: false,
+					color: '#000000',
+					connectorColor: '#000000'
+				}
+			}]
+		});
+		
+	});
+
